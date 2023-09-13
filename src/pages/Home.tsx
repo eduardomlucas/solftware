0e420b59dca2a0f42fa1587c822a72018d3efd11
eduardomlucas <eduardomachadolucas1@gmail.com>
2023-09-12 23:07:11 -0300

    feat: layout home and menu

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,8 +4,16 @@ import { ReactComponent as Logo } from "../images/logo.svg";
 function Home  ()  {
     return(
       <>
-        <Logo></Logo>
-        <h1>Home</h1>
+        <div id="title">
+          <Logo id="logo"></Logo>
+          <h1>s o l f t w a r e</h1>
+        </div>
+        <div id="welcome">
+        <p>Bem-vindos ao Solftware! Estamos empolgados em compartilhar que estamos na fase inicial de criação de uma plataforma de software que busca revolucionar a maneira como atendemos às suas necessidades. Nossa missão é proporcionar a você uma experiência excepcional, pautada na facilidade e agilidade, em todos os aspectos da sua jornada digital. Desde a sua concepção até a sua utilização cotidiana, estamos comprometidos em tornar sua experiência simples, eficaz e satisfatória.</p>
+        <p>Sabemos que cada um de vocês possui necessidades únicas, e é por isso que estamos trabalhando arduamente para criar um ambiente digital versátil que atenderá a uma ampla gama de interesses e demandas. Quer você seja um profissional em busca de ferramentas de produtividade, um entusiasta da tecnologia à procura das últimas inovações ou alguém em busca de soluções cotidianas, nosso software foi projetado com você em mente. Acreditamos que a simplicidade e a eficiência podem coexistir, e estamos ansiosos para apresentar a vocês nossa visão transformadora.</p>
+        <p>Enquanto continuamos a construir e aperfeiçoar nosso software, convidamos você a se juntar a nós nesta jornada emocionante. Fique atento às atualizações e novidades que compartilharemos em breve, à medida que nos aproximamos do lançamento oficial. Seja parte deste processo e ajude-nos a moldar a forma como entregamos soluções digitais que realmente facilitam a sua vida. Obrigado por visitar nosso site, e estamos ansiosos para fornecer a você uma experiência de usuário excepcional, repleta de facilidade e agilidade em um futuro próximo!</p>
+        </div>
+        
       </>
     ) 
     
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -4,20 +4,17 @@ import React from "react";
 function Layout () {
     return (
       <>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-            <li>
-              <Link to="/register">Register</Link>
-            </li>
-          </ul>
-        </nav>
-  
+        <ul id="menu">
+          <li>
+            <Link to="/"><h5>Home</h5></Link>
+          </li>
+          <li>
+            <Link to="/login"><h5>Login</h5></Link>
+          </li>
+          <li>
+            <Link to="/register"><h5>Register</h5></Link>
+          </li>
+        </ul>
         <Outlet />
       </>
     )
